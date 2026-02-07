e7437469d90239f7d1c631e2bf0cd65ce9d6201b
lahen <lah.ennabo@gmail.com>
2026-02-07 18:59:55 +0100

    version 1

--- src/main/java/ext/DaoImplV2.java
+++ src/main/java/ext/DaoImplV2.java
@@ -8,7 +8,7 @@ public class DaoImplV2 implements IDao {
     @Override
     public double getData() {
         System.out.println("Version web service");
-        double t = 77;
-        return t;
+        double ta = 77;
+        return ta;
     }
 }
